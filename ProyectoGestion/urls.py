ee3303786c8b9463dcdba4a26f0e9d870afd6a69
mariana.nunez <mariana.nunez10@gmail.com>
2020-08-26 20:01:45 -0300

    Clase del 26/08/2020

--- ProyectoGestion/urls.py
+++ ProyectoGestion/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 from ProyectoGestion import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hora/<int:horas>', views.saludo, name='saludo'),
--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from usuario.models import Usuario, Producto
+
+def modificar_email(modeladmin, request, queryset):
+	queryset.update(email='email@test.com')
+	modificar_email.short_description = "Modificame el email"
+
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'email')
+	actions = [modificar_email]
+
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Producto)
